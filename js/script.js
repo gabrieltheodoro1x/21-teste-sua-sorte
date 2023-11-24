706f4400c24a1922215ee0c0ad1904370fe5c959
gabr1el1x <85021555+gabrieltheodoro1x@users.noreply.github.com>
2023-11-24 18:29:28 -0300

    Reformulando o jogo

--- js/script.js
+++ js/script.js
@@ -1,383 +1,129 @@
-let bot = document.getElementById("botao");
-let pass = document.getElementById("passar");
-let somaPlayer = document.getElementById("soma");
-let somaComputador = document.getElementById("somac");
-let breaks = 0;
-let numbers = [];
-let numbersComputador = [];
 
+let nj = document.getElementById('novoJogo');
+let cp = document.getElementById('comprar');
+let pa = document.getElementById('parar');
+let res = document.getElementById('resultado');
 
-function vencedor(){
-    let p = 0;
-    let c = 0;
-    if(breaks >= 2){
-        if(somaPlayer <= 21){
-            p = 1;
-        }else if(somaPlayer > 21){
-            p = 2;
-        }
+nj.addEventListener('click', iniciarJogo);
+cp.addEventListener('click', comprar);
+pa.addEventListener('click', parar);
 
-        if(somaComputador <= 21){
-            c = 1;
-        }else if(somaComputador > 21){
-            c = 2;
-        }
+let pontuacaoJogador, pontuacaoComputador, cartasJogador, cartasComputador, jogoEncerrado;
 
-        if(p == 1 && c == 2){
-            window.alert("Vc ganhou");
-        }else if(p == 2 && c == 1){
-            window.alert("Vc perdeu");
-        }else if(p == 2 && c == 2){
-            if(p>c){
-                window.alert("Vc perdeu"); 
-            }else{
-                window.alert("Vc ganhou");
-            }
-        }else if(p == 1 && c == 1){
-            if(p>c){
-                window.alert("Vc ganhou"); 
-            }else{
-                window.alert("Vc perdeu");
-            }
-        }
+function iniciarJogo() {
+    pontuacaoJogador = 0;
+    pontuacaoComputador = 0;
+    cartasJogador = [];
+    cartasComputador = [];
+    jogoEncerrado = false;
+    res.innerHTML = '';
+    nj.disabled = true;
+    cp.disabled = false;
+    pa.disabled = false;
+
+    cartasJogador.push(obterProximaCarta());
+    cartasJogador.push(obterProximaCarta());
+    cartasComputador.push(obterProximaCarta());
+
+    atualizarPontuacoes();
+}
+
+function obterProximaCarta() {
+    const cartas = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
+    const indiceAleatorio = Math.floor(Math.random() * cartas.length);
+    const carta = cartas[indiceAleatorio];
+    return carta;
+}
+
+function obterValorCarta(carta) {
+    if (carta === 'K' || carta === 'Q' || carta === 'J') {
+        return 10;
+    } else if (carta === 'A') {
+        return 11;
+    } else {
+        return parseInt(carta);
     }
 }
 
-let sim;
-let nao;
+function atualizarPontuacoes() {
+    pontuacaoJogador = calcularPontuacao(cartasJogador);
+    pontuacaoComputador = calcularPontuacao(cartasComputador);
 
+    document.getElementById('pontuacaoJogador').innerHTML = 'Sua pontuação: ' + pontuacaoJogador;
+    document.getElementById('pontuacaoComputador').innerHTML = 'Pontuação do Computador: ' + pontuacaoComputador;
 
-bot.addEventListener('click', function f(){
-    if(breaks >= 2){
-        vencedor();
-        bot.removeEventListener('click', f);
-    }
-
-    breaks +=1;
-    let num = Math.random();
-    let numc = Math.random();
-
-    sim = num;
-    nao = numc;
-
-    //player
-    if(num>0 && num<0.10){
-        numbers.push(1);
-    }
-    else if(num>=0 && num<=0.10){
-        numbers.push(2);
-    }
-    else if(num>0.1 && num<=0.20){
-        numbers.push(3);
-    }
-    else if(num>0.2 && num<=0.30){
-        numbers.push(4);
-    }
-    else if(num>0.3 && num<=0.40){
-        numbers.push(5);
-    }
-    else if(num>0.4 && num<=0.50){
-        numbers.push(6);
-    }
-    else if(num>0.5 && num<=0.60){
-        numbers.push(7);
-    }
-    else if(num>0.6 && num<=0.70){
-        numbers.push(8);
-    }
-    else if(num>0.7 && num<=0.80){
-        numbers.push(9);
-    }
-    else if(num>0.8 && num<=0.90){
-        numbers.push(10);
-    }else if(num>0.9 && num<=1){
-        numbers.push(11);
-    }
-    //computador
-    if(numc>0 && numc<0.10){
-        numbersComputador.push(1);
-    }
-    else if(numc>=0 && numc<=0.10){
-        numbersComputador.push(2);
-    }
-    else if(numc>0.1 && numc<=0.20){
-        numbersComputador.push(3);
-    }
-    else if(numc>0.2 && numc<=0.30){
-        numbersComputador.push(4);
-    }
-    else if(numc>0.3 && numc<=0.40){
-        numbersComputador.push(5);
-    }
-    else if(numc>0.4 && numc<=0.50){
-        numbersComputador.push(6);
-    }
-    else if(numc>0.5 && numc<=0.60){
-        numbersComputador.push(7);
-    }
-    else if(numc>0.6 && numc<=0.70){
-        numbersComputador.push(8);
-    }
-    else if(numc>0.7 && numc<=0.80){
-        numbersComputador.push(9);
-    }
-    else if(numc>0.8 && numc<=0.90){
-        numbersComputador.push(10);
-    }else if(numc>0.9 && numc<=1){
-        numbersComputador.push(11);
+    if (pontuacaoJogador === 21 && cartasJogador.length === 2) {
+        res.innerHTML = 'Blackjack! Você venceu!';
+        jogoEncerrado = true;
+        encerrarJogo();
     }
 
-    let s = 0;
-    let sc = 0;
-    for(let i = 0; i<numbers.length; i++){
-        s+= numbers[i];
-        sc+= numbersComputador[i];
+    if (pontuacaoJogador > 21) {
+        res.innerHTML = 'Você estourou! Computador vence!';
+        jogoEncerrado = true;
+        encerrarJogo();
     }
-    somaPlayer.innerHTML = s;
-    somaComputador.innerHTML = sc;
 
-})
-
-pass.addEventListener('click', function(){
-    if(breaks >= 2){
-        vencedor();
-    }
-    breaks +=1;
-})
-/*
-let bot = document.getElementById("botao");
-let pass = document.getElementById("passar");
-let somaPlayer = document.getElementById("soma");
-let somaComputador = document.getElementById("somac");
-let breaks = 0;
-let numbers = [];
-let numbersComputador = [];
-
-
-function vencedor(){
-    let p = 0;
-    let c = 0;
-    if(breaks >= 2){
-        if(sp <= 21){
-            p = 1;
-        }else if(sp > 21){
-            p = 2;
-        }
-
-        if(scc <= 21){
-            c = 1;
-        }else if(scc > 21){
-            c = 2;
-        }
-
-        if(p == 1 && c == 2){
-            window.alert("Vc ganhou");
-        }else if(p == 2 && c == 1){
-            window.alert("Vc perdeu");
-        }else if(p == 2 && c == 2){
-            if(sp>scc){
-                window.alert("Vc perdeu"); 
-            }else{
-                window.alert("Vc ganhou");
-            }
-        }else if(p == 1 && c == 1){
-            if(sp>scc){
-                window.alert("Vc ganhou"); 
-            }else{
-                window.alert("Vc perdeu");
-            }
-        }
+    if (pontuacaoComputador === 21) {
+        res.innerHTML = 'Computador fez Blackjack. Você perdeu!';
+        jogoEncerrado = true;
+        encerrarJogo();
     }
 }
 
-let sim;
-let nao;
+function calcularPontuacao(cartas) {
+    let pontuacao = 0;
+    let countAs = 0;
 
+    for (const carta of cartas) {
+        pontuacao += obterValorCarta(carta);
 
-bot.addEventListener('click', function f(){
-    if(breaks < 3){
-
-        breaks +=1;
-        let num = Math.random();
-        let numc = Math.random();
-
-        sim = num;
-        nao = numc;
-
-        //player
-        if(num>0 && num<0.10){
-            numbers.push(1);
+        if (carta === 'A') {
+            countAs++;
         }
-        else if(num>=0 && num<=0.10){
-            numbers.push(2);
-        }
-        else if(num>0.1 && num<=0.20){
-            numbers.push(3);
-        }
-        else if(num>0.2 && num<=0.30){
-            numbers.push(4);
-        }
-        else if(num>0.3 && num<=0.40){
-            numbers.push(5);
-        }
-        else if(num>0.4 && num<=0.50){
-            numbers.push(6);
-        }
-        else if(num>0.5 && num<=0.60){
-            numbers.push(7);
-        }
-        else if(num>0.6 && num<=0.70){
-            numbers.push(8);
-        }
-        else if(num>0.7 && num<=0.80){
-            numbers.push(9);
-        }
-        else if(num>0.8 && num<=0.90){
-            numbers.push(10);
-        }else if(num>0.9 && num<=1){
-            numbers.push(11);
-        }
-        //computador
-        if(numc>0 && numc<0.10){
-            numbersComputador.push(1);
-        }
-        else if(numc>=0 && numc<=0.10){
-            numbersComputador.push(2);
-        }
-        else if(numc>0.1 && numc<=0.20){
-            numbersComputador.push(3);
-        }
-        else if(numc>0.2 && numc<=0.30){
-            numbersComputador.push(4);
-        }
-        else if(numc>0.3 && numc<=0.40){
-            numbersComputador.push(5);
-        }
-        else if(numc>0.4 && numc<=0.50){
-            numbersComputador.push(6);
-        }
-        else if(numc>0.5 && numc<=0.60){
-            numbersComputador.push(7);
-        }
-        else if(numc>0.6 && numc<=0.70){
-            numbersComputador.push(8);
-        }
-        else if(numc>0.7 && numc<=0.80){
-            numbersComputador.push(9);
-        }
-        else if(numc>0.8 && numc<=0.90){
-            numbersComputador.push(10);
-        }else if(numc>0.9 && numc<=1){
-            numbersComputador.push(11);
-        }
-
-        let s = 0;
-        let sc = 0;
-        for(let i = 0; i<numbers.length; i++){
-            s+= numbers[i];
-            sc+= numbersComputador[i];
-        }
-        sp = s;
-        scc = sc;
-        somaPlayer.innerHTML = s;
-        somaComputador.innerHTML = sc;
-    }else{
-
-        breaks +=1;
-        let num = Math.random();
-        let numc = Math.random();
-
-        sim = num;
-        nao = numc;
-
-        //player
-        if(num>0 && num<0.10){
-            numbers.push(1);
-        }
-        else if(num>=0 && num<=0.10){
-            numbers.push(2);
-        }
-        else if(num>0.1 && num<=0.20){
-            numbers.push(3);
-        }
-        else if(num>0.2 && num<=0.30){
-            numbers.push(4);
-        }
-        else if(num>0.3 && num<=0.40){
-            numbers.push(5);
-        }
-        else if(num>0.4 && num<=0.50){
-            numbers.push(6);
-        }
-        else if(num>0.5 && num<=0.60){
-            numbers.push(7);
-        }
-        else if(num>0.6 && num<=0.70){
-            numbers.push(8);
-        }
-        else if(num>0.7 && num<=0.80){
-            numbers.push(9);
-        }
-        else if(num>0.8 && num<=0.90){
-            numbers.push(10);
-        }else if(num>0.9 && num<=1){
-            numbers.push(11);
-        }
-        //computador
-        if(numc>0 && numc<0.10){
-            numbersComputador.push(1);
-        }
-        else if(numc>=0 && numc<=0.10){
-            numbersComputador.push(2);
-        }
-        else if(numc>0.1 && numc<=0.20){
-            numbersComputador.push(3);
-        }
-        else if(numc>0.2 && numc<=0.30){
-            numbersComputador.push(4);
-        }
-        else if(numc>0.3 && numc<=0.40){
-            numbersComputador.push(5);
-        }
-        else if(numc>0.4 && numc<=0.50){
-            numbersComputador.push(6);
-        }
-        else if(numc>0.5 && numc<=0.60){
-            numbersComputador.push(7);
-        }
-        else if(numc>0.6 && numc<=0.70){
-            numbersComputador.push(8);
-        }
-        else if(numc>0.7 && numc<=0.80){
-            numbersComputador.push(9);
-        }
-        else if(numc>0.8 && numc<=0.90){
-            numbersComputador.push(10);
-        }else if(numc>0.9 && numc<=1){
-            numbersComputador.push(11);
-        }
-
-        let s = 0;
-        let sc = 0;
-        for(let i = 0; i<numbers.length; i++){
-            s+= numbers[i];
-            sc+= numbersComputador[i];
-        }
-        sp = s;
-        scc = sc;
-        somaPlayer.innerHTML = s;
-        somaComputador.innerHTML = sc;
-        vencedor();
     }
-});
 
-
-
-pass.addEventListener('click', function(){
-    if(breaks == 3){
-        vencedor();
+    while (countAs > 0 && pontuacao > 21) {
+        pontuacao -= 10;
+        countAs--;
     }
-    breaks +=1;
-})
 
+    return pontuacao;
+}
+
+
+function comprar() {
+    if (jogoEncerrado == true) return;
+
+    cartasJogador.push(obterProximaCarta());
+    atualizarPontuacoes();
+}
+
+function parar() {
+    if (jogoEncerrado == true) return;
+
+    while (pontuacaoComputador < 17) {
+        cartasComputador.push(obterProximaCarta());
+        atualizarPontuacoes();
+    }
+
+    if (pontuacaoComputador > 21) {
+        res.innerHTML  = 'Computador estourou. Você venceu!';
+    } else if (pontuacaoComputador > pontuacaoJogador) {
+        res.innerHTML  = 'Computador vence!';
+    } else if (pontuacaoComputador < pontuacaoJogador) {
+        res.innerHTML = 'Você venceu!';
+    } else {
+        res.innerHTML  = 'Empate!';
+    }
+
+    jogoEncerrado = true;
+    encerrarJogo();
+}
+
+function encerrarJogo() {
+    nj.disabled = false;
+    cp.disabled = true;
+    pa.disabled = true;
+}
 
